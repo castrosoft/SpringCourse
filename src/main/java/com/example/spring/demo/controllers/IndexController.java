77c94bacb3cd80adb8e1fb53e8193c4702fe6b01
Piter Castro <castro.fcefyn@gmail.com>
2020-08-19 23:40:59 -0300

    Agrego @RequestMapping sobre el controller. Se crea ruta de 1er nivel. Entrar con http://localhost:8080/app/

--- src/main/java/com/example/spring/demo/controllers/IndexController.java
+++ src/main/java/com/example/spring/demo/controllers/IndexController.java
@@ -2,28 +2,19 @@ package com.example.spring.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
 @Controller
+@RequestMapping("/app") //localhost:8080/app/index
 public class IndexController {
 
-    //@RequestMapping(value="/index", method = RequestMethod.GET)
-    //@RequestMapping(value="/index")
-    //@GetMapping(value="/index")
-    @GetMapping("/index")
-    //@GetMapping({"/index", "/", "/home"})
-    //@PostMapping(value="/index")
-    public ModelAndView index(ModelAndView mv){
-
-        mv.addObject("titulo", "Hola Spring FrameWork con ModelAndView");
-        mv.setViewName("index");
-        return mv;
+    @GetMapping({"/index", "/", "", "/home"})
+    public String index(Model model){
+        model.addAttribute("titulo", "Hola Spring Framework con Model");
+        return "index";
     }
 }

ba5c52a7a6196d16f36922509e6349c9f2e90e35
Piter Castro <castro.fcefyn@gmail.com>
2020-08-19 21:44:54 -0300

    Creo un controller basico y la vista index correspondiente

---
+++ src/main/java/com/example/spring/demo/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package com.example.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    //@RequestMapping(value="/index", method = RequestMethod.GET)
+    //@RequestMapping(value="/index")
+    //@GetMapping(value="/index")
+    @GetMapping("/index")
+    //@GetMapping({"/index", "/", "/home"})
+    //@PostMapping(value="/index")
+    public String index(){
+
+        return "index";
+    }
+}

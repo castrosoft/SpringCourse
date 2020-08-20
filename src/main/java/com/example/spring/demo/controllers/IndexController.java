ef946287042b1d053e449d5edce6bfe5113a8a92
Piter Castro <castro.fcefyn@gmail.com>
2020-08-19 22:04:55 -0300

    Envio datos a la vista, pasando como parametro el ModelMap en el metodo index.

--- src/main/java/com/example/spring/demo/controllers/IndexController.java
+++ src/main/java/com/example/spring/demo/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.example.spring.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class IndexController {
     @GetMapping("/index")
     //@GetMapping({"/index", "/", "/home"})
     //@PostMapping(value="/index")
-    public String index(Model model){
+    public String index(ModelMap model){
 
         model.addAttribute("titulo", "Hola Spring FrameWork");
         return "index";

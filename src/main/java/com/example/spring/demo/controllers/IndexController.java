422e019375efd0db7b446c8f60be6d689a44b367
Piter Castro <castro.fcefyn@gmail.com>
2020-08-22 11:33:04 -0300

    Uso de @Qualifier en el controller

--- src/main/java/com/example/spring/demo/controllers/IndexController.java
+++ src/main/java/com/example/spring/demo/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.example.spring.demo.controllers;
 import com.example.spring.demo.models.services.IndexInterface;
 import com.example.spring.demo.models.services.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class IndexController {
     //private IndexService myService = new IndexService();
 
     @Autowired
+    @Qualifier("myComplexService")
     private IndexInterface myService;
 
     @GetMapping({"/index", "/", "/home"})
